e16136dfbae45a6cd52b153ac4c23e34d0120653
Ngugi Ndung'u <ngugindunguapps@gmail.com>
2016-01-28 20:52:37 +0300

    Add Mainpage.xaml

--- Pepeza/App.xaml.cs
+++ Pepeza/App.xaml.cs
@@ -92,7 +92,7 @@ namespace Pepeza
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(Pepeza.Views.LoginPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
